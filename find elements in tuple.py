fb51f31bc6c405e5b2ae960ce748d9b3459a1fe9
Nagarjuna Reddy <nr8390013@gmail.com>
2026-07-13 13:27:47 +0530

    Add files via upload

---
+++ find elements in tuple.py
@@ -0,0 +1,16 @@
+
+
+#find elements in tuple
+
+
+tupl_1 = (999,45,67,8,9)
+
+find = 9
+
+for i in range(0,len(tupl_1)):
+    if(find == tupl_1[i]):
+        
+        print ("found element at index:",i)
+        
+        
+        break
---
+++ tuple 3.py
@@ -0,0 +1,17 @@
+
+
+tupl = (1,2,2,2.5,6,8)
+
+
+count = 0
+
+
+for i in range(0,len(tupl)):
+    if (2 == tupl[i]):
+        
+        
+         count = count+1
+         
+         print(" 2 occured in", count,"times")
+         
+       
