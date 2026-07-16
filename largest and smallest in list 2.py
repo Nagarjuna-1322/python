2d7d79be9d344b7afe88ba692dc962cd6e786b56
Nagarjuna Reddy <nr8390013@gmail.com>
2026-07-16 20:12:38 +0530

    Add files via upload

---
+++ largest and smallest in list 2.py
@@ -0,0 +1,18 @@
+
+
+
+
+def find_largest_and_smallest(input_list):
+    max = 0
+    
+    for i in input_list:
+        if(max<i):
+            max = 5
+            return max
+                
+                
+
+input_list = [2,5,1,3]
+
+print(find_largest_and_smallest(input_list))
+
