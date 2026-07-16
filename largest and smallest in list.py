c0d4a99dec3f2fdd62d4172ed4b7795643e6c752
Nagarjuna Reddy <nr8390013@gmail.com>
2026-07-16 20:12:17 +0530

    Add files via upload

---
+++ largest and smallest in list.py
@@ -0,0 +1,13 @@
+
+
+
+#largest elements in the tuple
+
+
+def find_largest_and_smallest(input_list):
+    return max(input_list)
+
+input_list = [2,5,1,3]
+
+print(find_largest_and_smallest(input_list))
+
