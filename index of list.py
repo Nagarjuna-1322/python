48c7cc9162631a7661fd8152cf8b42404d41617d
Nagarjuna Reddy <nr8390013@gmail.com>
2026-07-24 19:42:32 +0530

    Add files via upload

---
+++ index of list.py
@@ -0,0 +1,16 @@
+
+
+
+#Find the Index of an Element in a List
+
+# List
+numbers = [10, 20, 30, 40]
+
+# Element to find
+element = 30
+
+# Find index
+if element in numbers:
+    print("Index:", numbers.index(element))
+else:
+    print("Element not found")
