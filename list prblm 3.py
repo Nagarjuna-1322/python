e868175f445f812d2ebe9f41b330da29cee109f8
Nagarjuna Reddy <nr8390013@gmail.com>
2026-08-19 17:31:56 +0530

    updates the comments

--- list prblm 3.py
+++ list prblm 3.py
@@ -5,3 +5,6 @@ numbers = [10, 25, 8, 45, 32]
 smallest = min(numbers)
 
 print("smallest element:", smallest)
+
+
+# the smallest is in terms of programming is minimum and the largest number is maximum
