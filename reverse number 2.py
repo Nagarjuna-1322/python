d4c8f2e1c531861b29937e146f4bfe2850070bb9
Nagarjuna Reddy <nr8390013@gmail.com>
2026-09-14 14:19:15 +0530

    Add files via upload

---
+++ reverse number 2.py
@@ -0,0 +1,15 @@
+
+
+
+
+# Reverse a number using a loop.
+
+n = int(input("Enter a number: "))
+rev = 0
+
+while n > 0:
+    digit = n % 10
+    rev = rev * 10 + digit
+    n //= 10
+
+print("Reversed number:", rev)
