268a80f609e5fb4816671907ac349bacdeb15da9
Nagarjuna Reddy <nr8390013@gmail.com>
2026-09-17 12:07:51 +0530

    Add files via upload

---
+++ distance time.py
@@ -0,0 +1,15 @@
+
+
+nyc_bal = 188
+bal_pitt = 247
+
+total_distance = nyc_bal+bal_pitt
+print(total_distance)
+
+mph = 65
+time = total_distance/mph
+
+
+print(round(time,2))
+
+#the rounbd keyword is used to print the less nearest value 
