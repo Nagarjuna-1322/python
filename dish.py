12ee6e052004f746836f72d5a56ac34167111eed
Nagarjuna Reddy <nr8390013@gmail.com>
2026-09-23 18:18:21 +0530

    Add files via upload

---
+++ dish.py
@@ -0,0 +1,21 @@
+
+
+
+indian = ["samosa","dall","naan"]
+chinese = ["egg role", "pot sticker","fried rice"]
+
+italian = ["pizza", "pasta:,ristoto"]
+
+
+dish = input(" enter the dish name")
+
+if dish in indian:
+    print("indian")
+elif dish in chinese:
+    print("chinese")
+    
+elif dish in italian:
+    print("italian")
+    
+else:
+    print("not these countrys")
